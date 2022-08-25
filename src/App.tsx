87924714d2a02840aaec4921419134a29980dd68
Nycollas Pontes <nycollaspontes@gmail.com>
2022-08-25 11:37:09 -0300

    Add Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-query {
-  lessons {
-    id 
-    title
-  }
-}
-`
-interface Lesson {
-  id : string;
-  title : string; 
-}
+import { Event } from "./pages/Events"
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
-  console.log(data)
   return (
-    <ul>
-      {data.lessons.map((lesson:Lesson) => {
-        return <li>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+    <Event />
+    </div>
   )
 }
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export function Header(){
+    return (
+        <h1 className="w-max-auto flex justify-center h-16 ">Header</h1>
+    )
+}
