846dedc35ad28782a0bc539904bc30659c01f2cf
Nycollas Pontes <nycollaspontes@gmail.com>
2022-09-08 00:14:43 -0300

    Add mutation createSubscriber and permissions

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:"https://api-sa-east-1.hygraph.com/v2/cl78hpjq80u8n01t2fnikd24d/master",
+    uri: import.meta.env.VITE_URL,
+    headers : {
+        Autorizathion : `Bearer ${import.meta.env.VITE_ACESS_TOKEN} `
+    },
     cache: new InMemoryCache()
 })
